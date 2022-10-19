0fed9e433e2153d8e52528a91d47f4944f523680
roaa94 <roaa94khaddam@gmail.com>
2022-10-19 21:24:58 +0300

    chore(core): adds fonts and text styles

--- packages/core/lib/src/styles/app_themes.dart
+++ packages/core/lib/src/styles/app_themes.dart
@@ -1,4 +1,5 @@
 import 'package:core/src/styles/app_colors.dart';
+import 'package:core/src/styles/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 /// Helper class for app wide themes
@@ -7,7 +8,20 @@ class AppThemes {
   static ThemeData getTheme({bool isDark = false}) {
    return ThemeData(
      brightness: isDark ? Brightness.light : Brightness.dark,
+     fontFamily: AppTextStyles.fontFamily,
      primaryColor: AppColors.getMaterialColorFromColor(AppColors.primary),
+     textTheme: const TextTheme(
+       headline1: AppTextStyles.h1,
+       headline2: AppTextStyles.h2,
+       headline3: AppTextStyles.h3,
+       headline4: AppTextStyles.h4,
+       headline5: AppTextStyles.h5,
+       caption: AppTextStyles.caption,
+       bodyText1: AppTextStyles.bodyLg,
+       bodyText2: AppTextStyles.body,
+       subtitle1: AppTextStyles.bodySmBold,
+       subtitle2: AppTextStyles.bodySm,
+     ),
    );
   }
 }
