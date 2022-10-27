3fd092b8db094bbbf031dc63d6ef1bef3f9df19d
roaa94 <roaa94khaddam@gmail.com>
2022-10-27 20:41:05 +0300

    refactor(core): uses TweetMetric widget with Replies widget

--- packages/core/lib/src/tweet/widgets/replies.dart
+++ packages/core/lib/src/tweet/widgets/replies.dart
@@ -1,10 +1,9 @@
 import 'package:core/src/styles/app_colors.dart';
-import 'package:core/src/tweet/widgets/metric_text.dart';
-import 'package:core/src/widgets/app_icon_button.dart';
+import 'package:core/src/tweet/widgets/tweet_metric.dart';
 import 'package:flutter/material.dart';
 
 /// Tweet replies icon and metric text widget
-class Replies extends StatefulWidget {
+class Replies extends StatelessWidget {
   /// Creates new instance of [Replies]
   const Replies({
     super.key,
@@ -19,37 +18,15 @@ class Replies extends StatefulWidget {
   final double iconSize;
 
   @override
-  State<Replies> createState() => _RepliesState();
-}
-
-class _RepliesState extends State<Replies> {
-  bool _isActive = false;
-
-  @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        AppIconButton(
-          isActive: _isActive,
-          size: widget.iconSize,
-          // Todo: change to design icons
-          icon: const Icon(Icons.mode_comment_outlined),
-          activeIcon: const Icon(Icons.mode_comment),
-          hoverColor: AppColors.success,
-          activeColor: AppColors.success,
-          onPressed: () {
-            setState(() {
-              _isActive = !_isActive;
-            });
-          },
-        ),
-        MetricText(
-          value: widget.metricValue,
-          isActive: _isActive,
-          activeColor: AppColors.success,
-        ),
-      ],
+    return TweetMetric(
+      // Todo: change to design icons
+      icon: const Icon(Icons.mode_comment_outlined),
+      activeIcon: const Icon(Icons.mode_comment),
+      activeColor: AppColors.success,
+      onChanged: (value) {
+        // Todo: implement reply action
+      },
     );
   }
 }
