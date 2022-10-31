2771a4fb007907e39cfe72c225855d67a5caffeb
roaa94 <roaa94khaddam@gmail.com>
2022-10-31 15:44:22 +0300

    feat(l10n): ✨ set up localization in core package with `en` and `ar` locales

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -1,5 +1,6 @@
 /// Core components for Widgetbook Demo App
 library core;
 
+export './src/l10n/l10n.dart';
 export './src/styles/styles.dart';
 export './src/widgets/widgets.dart';
---
+++ packages/core/lib/src/l10n/app_localizations_ar.dart
@@ -0,0 +1,12 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get widgetbook => 'Widgetbook';
+
+  @override
+  String get hi => 'مرحباً';
+}
