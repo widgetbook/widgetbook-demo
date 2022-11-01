71af5335070ee126802bdcf648fc8de269fa9127
roaa94 <roaa94khaddam@gmail.com>
2022-11-01 22:00:11 +0300

    chore(theme): fixes some text style colors

--- packages/core/lib/src/styles/app_themes.dart
+++ packages/core/lib/src/styles/app_themes.dart
@@ -6,36 +6,46 @@ import 'package:flutter/material.dart';
 class AppThemes {
   /// Returns theme data
   static ThemeData getTheme({bool isDark = false}) {
-   return ThemeData(
-     brightness: isDark ? Brightness.dark : Brightness.light,
-     fontFamily: AppTextStyles.fontFamily,
-     primaryColor: AppColors.getMaterialColorFromColor(AppColors.primary),
-     scaffoldBackgroundColor: isDark ? AppColors.black : AppColors.white,
-     dividerColor: AppColors.border,
-     elevatedButtonTheme: ElevatedButtonThemeData(
-       style: ElevatedButton.styleFrom(
-         shape: RoundedRectangleBorder(
-           borderRadius: BorderRadius.circular(100),
-         ),
-         elevation: 0,
-         textStyle: AppTextStyles.button,
-       ),
-     ),
-     iconTheme: IconThemeData(
-       color: isDark ? AppColors.whiteLight : AppColors.textLight,
-     ),
-     textTheme: const TextTheme(
-       headline1: AppTextStyles.h1,
-       headline2: AppTextStyles.h2,
-       headline3: AppTextStyles.h3,
-       headline4: AppTextStyles.h4,
-       headline5: AppTextStyles.h5,
-       caption: AppTextStyles.caption,
-       bodyText1: AppTextStyles.bodyLg,
-       bodyText2: AppTextStyles.body,
-       subtitle1: AppTextStyles.bodySmBold,
-       subtitle2: AppTextStyles.bodySm,
-     ),
-   );
+    return ThemeData(
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      fontFamily: AppTextStyles.fontFamily,
+      primaryColor: AppColors.getMaterialColorFromColor(AppColors.primary),
+      scaffoldBackgroundColor: isDark ? AppColors.black : AppColors.white,
+      dividerColor: AppColors.border,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+          elevation: 0,
+          textStyle: AppTextStyles.button,
+        ),
+      ),
+      iconTheme: IconThemeData(
+        color: isDark ? AppColors.whiteLight : AppColors.textLight,
+      ),
+      textTheme: TextTheme(
+        headline1: AppTextStyles.h1,
+        headline2: AppTextStyles.h2,
+        headline3: AppTextStyles.h3,
+        headline4: AppTextStyles.h4,
+        headline5: AppTextStyles.h5,
+        caption: AppTextStyles.caption.copyWith(
+          color: isDark ? AppColors.whiteLight : AppColors.textLight,
+        ),
+        bodyText1: AppTextStyles.bodyLg.copyWith(
+          color: isDark ? AppColors.whiteLight : AppColors.textLight,
+        ),
+        bodyText2: AppTextStyles.body.copyWith(
+          color: isDark ? AppColors.whiteLight : AppColors.textLight,
+        ),
+        subtitle1: AppTextStyles.bodySmBold.copyWith(
+          color: isDark ? AppColors.whiteLight : AppColors.textLight,
+        ),
+        subtitle2: AppTextStyles.bodySm.copyWith(
+          color: isDark ? AppColors.whiteLight : AppColors.textLight,
+        ),
+      ),
+    );
   }
 }
