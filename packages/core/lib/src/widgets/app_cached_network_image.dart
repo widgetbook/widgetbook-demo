4046c66cbd4b260df2325ddad4b346c7d08ca955
roaa94 <roaa94khaddam@gmail.com>
2022-11-03 19:39:38 +0300

    feat(core): ✨ adds AppCachedNetworkImage widget

---
+++ packages/core/lib/src/widgets/app_cached_network_image.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+/// Wrapper widget for [CachedNetworkImage] with customizable
+/// loader and error widget
+class AppCachedNetworkImage extends StatelessWidget {
+  /// Creates a new instance of [AppCachedNetworkImage]
+  const AppCachedNetworkImage({
+    super.key,
+    required this.imageUrl,
+    this.fit = BoxFit.cover,
+    this.alignment = Alignment.center,
+  });
+
+  /// The image url
+  final String imageUrl;
+
+  /// Image box fit
+  ///
+  /// Defaults to [BoxFit.cover]
+  final BoxFit? fit;
+
+  /// Image alignment
+  final Alignment alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      fit: fit,
+      alignment: alignment,
+    );
+  }
+}
