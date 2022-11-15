1f4f6704a4e5b2455a37c048d9f43a0601ec3750
roaa94 <roaa94khaddam@gmail.com>
2022-11-15 12:51:39 +0300

    chore: moves enums to separate files

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -5,4 +5,5 @@ export './src/l10n/l10n.dart';
 export './src/models/models.dart';
 export './src/styles/styles.dart';
 export './src/utils/utils.dart';
+export './src/enums/enums.dart';
 export './src/widgets/widgets.dart';
