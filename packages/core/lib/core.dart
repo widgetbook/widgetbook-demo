8f15308691904dec750c3cb46f36769cf3cadffd
roaa94 <roaa94khaddam@gmail.com>
2022-11-15 14:40:56 +0300

    chore: code format

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -1,9 +1,9 @@
 /// Core components for Widgetbook Demo App
 library core;
 
+export './src/enums/enums.dart';
 export './src/l10n/l10n.dart';
 export './src/models/models.dart';
 export './src/styles/styles.dart';
 export './src/utils/utils.dart';
-export './src/enums/enums.dart';
 export './src/widgets/widgets.dart';
