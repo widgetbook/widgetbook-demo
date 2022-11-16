4a352bc9ef9db2cf45867f3d387fac565449aed9
roaa94 <roaa94khaddam@gmail.com>
2022-11-16 13:06:17 +0300

    chore: adds MenuItem widget file

---
+++ packages/core/lib/src/widgets/ui/menu_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
