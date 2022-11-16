c84b9aaa6016727784cb83f9904c77f5eb9d2044
roaa94 <roaa94khaddam@gmail.com>
2022-11-16 12:54:08 +0300

    feat: ✨ adds dark theme support to Button component

--- packages/core/lib/src/widgets/ui/button.dart
+++ packages/core/lib/src/widgets/ui/button.dart
@@ -80,22 +80,47 @@ class Button extends StatelessWidget {
   /// Optional icon
   final IconData? icon;
 
+  Color? _getBackgroundColor(bool isDark) {
+    if (isDark) {
+      if (backgroundColor == AppColors.white) {
+        return AppColors.secondary;
+      } else if (backgroundColor == AppColors.secondary) {
+        return AppColors.white;
+      }
+    }
+    return backgroundColor;
+  }
+
+  Color? _getTextColor(bool isDark) {
+    if (isDark) {
+      if (textColor == AppColors.white) {
+        return AppColors.secondary;
+      } else if (textColor == AppColors.secondary) {
+        return AppColors.white;
+      }
+    }
+    return textColor;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final dynamicBackgroundColor = _getBackgroundColor(isDark);
+    final dynamicTextColor = _getTextColor(isDark);
+
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        foregroundColor: textColor,
+        backgroundColor: dynamicBackgroundColor,
+        foregroundColor: dynamicTextColor,
         fixedSize: Size.fromHeight(height),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(height / 2),
           side: borderSide,
         ),
-        textStyle: Theme.of(context)
-            .textTheme
-            .button!
-            .copyWith(height: icon == null ? 1.05 : null),
+        textStyle: Theme.of(context).textTheme.button!.copyWith(
+              height: icon == null ? 1.05 : null,
+            ),
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
