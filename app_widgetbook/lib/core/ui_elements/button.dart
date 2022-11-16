c4c49c7e3aa3129dbde06c878def0387c43394b0
roaa94 <roaa94khaddam@gmail.com>
2022-11-16 13:15:58 +0300

    feat: adds custom use case for Button component

--- app_widgetbook/lib/core/ui_elements/button.dart
+++ app_widgetbook/lib/core/ui_elements/button.dart
@@ -1,7 +1,93 @@
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
-import 'package:widgetbook/widgetbook.dart' show Knobs;
+import 'package:widgetbook/widgetbook.dart' show Knobs, Option;
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+/// Use case for a Primary Button style
+@WidgetbookUseCase(name: 'Custom', type: Button)
+Widget customButtonUseCase(BuildContext context) {
+  return Button(
+    label: context.knobs.text(
+      label: 'Label',
+      initialValue: 'Button',
+    ),
+    onPressed: () {},
+    borderColor: context.knobs.options<Color?>(
+      label: 'Border Color',
+      options: const [
+        Option(
+          label: 'None',
+          value: null,
+        ),
+        Option(
+          label: 'Primary',
+          value: AppColors.primary,
+        ),
+        Option(
+          label: 'Secondary',
+          value: AppColors.secondary,
+        ),
+        Option(
+          label: 'Pink',
+          value: AppColors.pink,
+        ),
+        Option(
+          label: 'White',
+          value: AppColors.white,
+        ),
+      ],
+    ),
+    backgroundColor: context.knobs.options<Color?>(
+      label: 'Background Color',
+      options: const [
+        Option(
+          label: 'Primary',
+          value: AppColors.primary,
+        ),
+        Option(
+          label: 'Secondary',
+          value: AppColors.secondary,
+        ),
+        Option(
+          label: 'Pink',
+          value: AppColors.pink,
+        ),
+        Option(
+          label: 'White',
+          value: AppColors.white,
+        ),
+      ],
+    ),
+    textColor: context.knobs.options<Color?>(
+      label: 'Text Color',
+      options: const [
+        Option(
+          label: 'White',
+          value: AppColors.white,
+        ),
+        Option(
+          label: 'Primary',
+          value: AppColors.primary,
+        ),
+        Option(
+          label: 'Secondary',
+          value: AppColors.secondary,
+        ),
+        Option(
+          label: 'Pink',
+          value: AppColors.pink,
+        ),
+      ],
+    ),
+    height: context.knobs.options<double>(
+      label: 'Size',
+      options: const [
+        Option(label: 'Default', value: 40),
+        Option(label: 'Large', value: 56),
+      ],
+    ),
+  );
+}
 
 /// Use case for a Primary Button style
 @WidgetbookUseCase(name: 'Primary Button', type: Button)
