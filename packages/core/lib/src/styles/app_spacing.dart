b35a59b5509a3b11a1342ea47699e272abf34418
roaa94 <roaa94khaddam@gmail.com>
2022-11-22 19:43:12 +0300

    feat: ✨ adds AppSpacing class

---
+++ packages/core/lib/src/styles/app_spacing.dart
@@ -0,0 +1,24 @@
+/// Styles class containing fixed spacing values
+/// to be reused for gaps, padding, margins, ..etc across the app widgets
+class AppSpacing {
+  /// A spacing of size xx-small
+  static const double xxs = 3;
+
+  /// A spacing of size x-small
+  static const double xs = 5;
+
+  /// A spacing of size small
+  static const double sm = 10;
+
+  /// A spacing of size medium
+  static const double md = 20;
+
+  /// A spacing of size large
+  static const double lg = 30;
+
+  /// A spacing of size x-large
+  static const double xl = 60;
+
+  /// A spacing of size xx-large
+  static const double xxl = 70;
+}
