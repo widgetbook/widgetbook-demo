d3abda4ca1a68e4fc7526555461ba5a2efed7835
roaa94 <roaa94khaddam@gmail.com>
2022-11-23 12:38:15 +0300

    chore: setup AppBottomNavigationBar

---
+++ packages/core/lib/src/widgets/ui/navigation/app_bottom_navigation_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavigationBar extends StatelessWidget {
+  const AppBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [],
+    );
+  }
+}
