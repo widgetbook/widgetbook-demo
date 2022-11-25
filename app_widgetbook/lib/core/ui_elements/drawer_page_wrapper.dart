d5cb1fbfaa2d47e7ca228618f06f52572b668023
roaa94 <roaa94khaddam@gmail.com>
2022-11-25 14:51:50 +0300

    refactor: drawer animation RTL and dynamic width

--- app_widgetbook/lib/core/ui_elements/drawer_page_wrapper.dart
+++ app_widgetbook/lib/core/ui_elements/drawer_page_wrapper.dart
@@ -1,23 +1,24 @@
 import 'package:app_widgetbook/dummy_data/dummy_users.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
-import 'package:widgetbook/widgetbook.dart' show Knobs, Option, Device;
+import 'package:widgetbook/widgetbook.dart' show Knobs;
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 @WidgetbookUseCase(name: 'Default', type: DrawerPageWrapper)
 Widget drawerPageWrapperDefaultUseCase(BuildContext context) {
-  return DrawerPageWrapper(
-    // Todo: remove these values when `MediaQuery` issue is resolved
-    // now the width and height here are the hardcoded values of an iPhone 12/13
-    width: 390 + DrawerUtils.width,
-    height: 844,
-    user: DummyUsers.widgetbook,
-    appBarTitle: Text(
-      context.knobs.text(
-        label: 'AppBar Title',
-        initialValue: 'Home',
-      ),
+  return MediaQuery(
+    data: MediaQuery.of(context).copyWith(
+      size: const Size(390, 844),
     ),
-    body: Container(),
+    child: DrawerPageWrapper(
+      user: DummyUsers.widgetbook,
+      appBarTitle: Text(
+        context.knobs.text(
+          label: 'AppBar Title',
+          initialValue: 'Home',
+        ),
+      ),
+      body: Container(),
+    ),
   );
 }
