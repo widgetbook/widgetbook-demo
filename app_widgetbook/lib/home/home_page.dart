518301332fcd14755126bf6714488411866e183a
Jens Horstmann <jens@widgetbook.io>
2022-12-22 09:40:38 +0100

    feat: :sparkles: add `designLink` to `HomePage`

--- app_widgetbook/lib/home/home_page.dart
+++ app_widgetbook/lib/home/home_page.dart
@@ -4,7 +4,12 @@ import 'package:app_widgetbook/dummy_data/dummy_users.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-@WidgetbookUseCase(name: 'Default', type: HomePage)
+@WidgetbookUseCase(
+  name: 'Default',
+  type: HomePage,
+  designLink:
+      'https://www.figma.com/file/taoQSMi6WeUgzEoZvZmHmI/Twitter-Clone---Widgetbook-Demo-App?node-id=245%3A5569&t=oryzXtTPwoD1v9cA-4',
+)
 Widget homePageDefaultUseCase(BuildContext context) {
   return MediaQuery(
     data: MediaQuery.of(context).copyWith(
