2997bf3789c97cc905c4de1ed7c5c0a95a75676f
Roaa94 <roaa94khaddam@gmail.com>
2023-04-11 16:40:38 +0300

    feat: add design link to auth pages

--- app_widgetbook/lib/auth/pages/password_page.dart
+++ app_widgetbook/lib/auth/pages/password_page.dart
@@ -3,14 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart' show Knobs, Option;
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-@WidgetbookUseCase(name: 'Default', type: PasswordPage)
+@WidgetbookUseCase(
+  name: 'Default',
+  type: PasswordPage,
+  designLink:
+      'https://www.figma.com/file/taoQSMi6WeUgzEoZvZmHmI/Widgetbook-Demo-App?node-id=326-6816&t=wNWmUVdFMwf9tsLL-4',
+)
 Widget passwordPageDefaultUseCase(BuildContext context) {
   return PasswordPage(
     userInput: context.knobs.text(
       label: 'Login User Input',
       description: 'The user input from the previous login step. '
           'This can be a phone number, email, or username',
-      initialValue: 'johndoe'
+      initialValue: 'johndoe',
     ),
   );
 }
